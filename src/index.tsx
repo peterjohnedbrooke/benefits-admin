d715d39dabc2c577f4d2f7f0425e5cbe755373e6
peterjohnedbrooke <john.edbrooke@gmail.com>
2026-05-19 15:58:15 +0100

    feat: configure redux store with saga middleware

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/global.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
+import "./styles/global.css";
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   );
 }
